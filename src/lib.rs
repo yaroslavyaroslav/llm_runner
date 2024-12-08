687f890d71cdbc1f7412e2816bce3263cf37b6a9
Yaroslav Yashin <yaroslav.yashin@gmail.com>
2024-12-08 01:24:29 +0100

    Cacher functions implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-// use pyo3::prelude::*;
+use pyo3::prelude::*;
 use serde::{Deserialize, Serialize};
 use serde_json::Error as SerdeError;
-use std::fs::{self, File, OpenOptions};
+use std::fs::{File, OpenOptions};
 use std::io::{BufRead, Write};
 use std::path::Path;
 pub mod types;
+use types::{AssistantMessage, OpenAIMessage};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Cacher {
@@ -13,16 +14,64 @@ pub struct Cacher {
     pub tokens_count_file: String,
 }
 
-impl Cacher {
-    pub fn new(name: Option<&str>) -> Self {
-        let cache_dir = std::env::var("HOME").unwrap()
-            + "/Library/Application Support/Sublime Text/Packages/OpenAI completion";
-        fs::create_dir_all(&cache_dir).unwrap();
+#[derive(Debug, Deserialize)]
+pub enum Message {
+    OpenAI(OpenAIMessage),
+    Assistant(AssistantMessage),
+}
 
-        let name_prefix = name.map_or(String::new(), |n| format!("{}_", n));
-        let history_file = format!("{}/{}chat_history.json", cache_dir, name_prefix);
-        let current_model_file = format!("{}/{}current_assistant.json", cache_dir, name_prefix);
-        let tokens_count_file = format!("{}/{}tokens_count.json", cache_dir, name_prefix);
+impl Cacher {
+    pub fn new(module_name: &str, name: Option<&str>) -> Self {
+        let cache_dir = Python::with_gil(|py| {
+            let sublime = py
+                .import(module_name)
+                .expect("Failed to import the sublime module");
+
+            sublime
+                .call_method0("cache_path")
+                .expect("Failed to call sublime.cache_path()")
+                .extract::<String>()
+                .expect("Failed to convert the Python result to a Rust String")
+        });
+
+        use std::path::{Path, PathBuf};
+
+        let (_cache_dir, history_file, current_model_file, tokens_count_file) =
+            if let Some(input) = name {
+                if Path::new(input).is_absolute() {
+                    let base_path = PathBuf::from(input);
+                    (
+                        base_path.to_string_lossy().into_owned(),
+                        base_path
+                            .join("chat_history.json")
+                            .to_string_lossy()
+                            .into_owned(),
+                        base_path
+                            .join("current_assistant.json")
+                            .to_string_lossy()
+                            .into_owned(),
+                        base_path
+                            .join("tokens_count.json")
+                            .to_string_lossy()
+                            .into_owned(),
+                    )
+                } else {
+                    let name_prefix = format!("{}_", input);
+                    (
+                        cache_dir.clone(),
+                        format!("{}/{}chat_history.json", cache_dir, name_prefix),
+                        format!("{}/{}current_assistant.json", cache_dir, name_prefix),
+                        format!("{}/{}tokens_count.json", cache_dir, name_prefix),
+                    )
+                }
+            } else {
+                (
+                    cache_dir.clone(),
+                    format!("{}/chat_history.json", cache_dir),
+                    format!("{}/current_assistant.json", cache_dir),
+                    format!("{}/tokens_count.json", cache_dir),
+                )
+            };
 
         Self {
             history_file,
@@ -37,7 +86,7 @@ impl Cacher {
         }
     }
 
-    pub fn read_entries(&self) -> Result<Vec<serde_json::Value>, SerdeError> {
+    pub fn read_entries(&self) -> Result<Vec<Message>, SerdeError> {
         self.check_and_create(&self.history_file);
         let file = File::open(&self.history_file).unwrap();
         let reader = std::io::BufReader::new(file);
@@ -45,19 +94,21 @@ impl Cacher {
 
         for line in reader.lines() {
             let line = line.unwrap();
-            let obj: serde_json::Value = serde_json::from_str(&line)?;
+            let obj: Message = serde_json::from_str(&line)?;
             entries.push(obj);
         }
 
         Ok(entries)
     }
 
-    pub fn write_entry(&self, entry: &serde_json::Value) {
+    pub fn write_entry<T: Serialize>(&self, entry: &T) {
         let mut file = OpenOptions::new()
             .append(true)
             .create(true)
             .open(&self.history_file)
             .unwrap();
-        writeln!(file, "{}", serde_json::to_string(entry).unwrap()).unwrap();
+
+        let entry_json = serde_json::to_string(entry).unwrap();
+        writeln!(file, "{}", entry_json).unwrap();
     }
 }
